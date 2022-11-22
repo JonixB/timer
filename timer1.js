8674a7eb253ecd138f4d7605d11272a86c92e47b
Nikko Bansil <bjohnnikko@gmail.com>
2022-11-22 13:59:45 -0800

    a code that makes the computer beep based on the number of secods given in the CLI

---
+++ timer1.js
@@ -0,0 +1,16 @@
+let arguments = process.argv.slice(2);
+console.log(arguments);
+let parsedArgs = 0;
+
+if (arguments.length !== 0 ){
+  for (const element of arguments) {
+    parsedArgs = Number(element);
+    if (isNaN(parsedArgs)) {
+      continue;
+    }
+    setTimeout(() => {
+      process.stdout.write('\x07');
+      // process.stdout.write('.');
+    }, element + '000');
+  }
+}
